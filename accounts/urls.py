c55dcd2229c26276e56341204dd261d8c8d6b08f
vpliasetskyi <vpliasetskyi@gmail.com>
2026-04-20 02:47:42 -0700

    modify urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name='landing'),
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
     path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
